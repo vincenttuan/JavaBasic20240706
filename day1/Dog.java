f096b34af630c8b7e1982b61e55a383c98d3fa24
VincentTuan <vincentjava@yahoo.com.tw>
2024-07-06 12:06:49 +0800

    Add files via upload

---
+++ day1/Dog.java
@@ -0,0 +1,12 @@
+package day1;
+
+public class Dog {
+	public static void main(String[] args) {
+		// 畫一隻狗
+		System.out.println("   / \\__     ");
+		System.out.println("  (    @\\___ ");
+		System.out.println("  /         O ");
+		System.out.println(" /   (_____/  ");
+		System.out.println(" /_____/   U  ");
+	}
+}
