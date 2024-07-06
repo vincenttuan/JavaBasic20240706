100f99843700612113d061bfd3beb05c441def9a
VincentTuan <vincentjava@yahoo.com.tw>
2024-07-06 14:43:35 +0800

    Add files via upload

---
+++ day2/Demo1.java
@@ -0,0 +1,17 @@
+package day2;
+
+/**
+ * 整數資料型別
+ * byte, short, int(預設), long
+ * */
+public class Demo1 {
+
+	public static void main(String[] args) {
+		System.out.println(100 + 90); // int 100 + int 90 = 190
+		System.out.println(100 - 90); // = 10
+		System.out.println(100 * 90); // = 9000
+		System.out.println(100 / 90); // = 1 (整數相除只會得到商)
+		System.out.println(100 % 90); // = 10 (餘數)
+	}
+
+}
