48ef060306d0cebfab09f50839bfbeb8c9832ab6
VincentTuan <vincentjava@yahoo.com.tw>
2024-07-06 12:31:56 +0800

    Add files via upload

---
+++ day1/Cat.java
@@ -0,0 +1,12 @@
+package day1;
+
+public class Cat {
+	public static void main(String[] args) {
+		// 畫一隻貓
+		System.out.println("   / \\ / \\     ");
+		System.out.println("    O   O       ");
+		System.out.println("  --     --     ");
+		System.out.println(" <--  .  --> ");
+		System.out.println("  --     --");
+	}
+}
