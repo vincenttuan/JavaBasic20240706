c5d278e65f99ff7d186cf0aacf15485e45c1aa7c
VincentTuan <vincentjava@yahoo.com.tw>
2024-07-13 10:40:44 +0800

    Add files via upload

---
+++ day3/Test4.java
@@ -0,0 +1,14 @@
+package day3;
+
+public class Test4 {
+
+	public static void main(String[] args) {
+		byte a = 10;
+		byte b = 20;
+		byte c = a + b;
+		System.out.printf("a = %d%n", a);
+		System.out.printf("b = %d%n", b);
+		System.out.printf("c = %d%n", c);
+	}
+
+}
