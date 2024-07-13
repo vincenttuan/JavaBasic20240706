7342e3f98c7e5adfe8974697e9424f1dd1b73007
VincentTuan <vincentjava@yahoo.com.tw>
2024-07-13 14:52:32 +0800

    Update Input3.java

--- day4/Input3.java
+++ day4/Input3.java
@@ -20,6 +20,36 @@ BMR=447.593+(9.247×體重(kg))+(3.098×身高(cm))−(4.330×年齡)
 
  * */
 
-public class Input3 {
+import java.util.Scanner;
 
+public class Input3 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("請輸入性別(男-1/女-2):");
+		int gender = sc.nextInt(); // 性別
+		System.out.print("請輸入年齡:");
+		int age = sc.nextInt();
+		System.out.print("請輸入身高(cm):");
+		double h = sc.nextDouble();
+		System.out.print("請輸入體重(kg):");
+		double w = sc.nextDouble();
+		// 印出使用者所輸入的資料
+		System.out.printf("性別: %d 年齡 :%d 身高: %.1f 體重: %.1f%n", gender, age, h, w);
+
+		// 計算 BMR
+		double bmr = 0;
+		if(gender == 1) { // 男性
+			bmr = 88.362+(13.397*w)+(4.799*h)-(5.677*age);
+		} else if(gender == 2) { // 女性
+			bmr = 447.593+(9.247*w)+(3.098*h)-(4.330*age);
+		} else {
+			System.out.println("性別輸入錯誤");
+			return; // main 方法提前結束
+		}
+
+		// 印出 bmr
+		System.out.println("男性正常: 1600~1800 大卡/天");
+		System.out.println("女性正常: 1400~1600 大卡/天");
+		System.out.printf("BMR: %.1f 大卡/天%n", bmr);
+	}
 }
