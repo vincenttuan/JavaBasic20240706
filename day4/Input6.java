0e95011428a0951f14a7681cd64e891a9a70d61f
VincentTuan <vincentjava@yahoo.com.tw>
2024-07-13 16:24:02 +0800

    Add files via upload

---
+++ day4/Input6.java
@@ -0,0 +1,36 @@
+package day4;
+
+import java.util.Scanner;
+
+public class Input6 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("請輸入身份證字號:");
+		String id = sc.next();
+		char no1 = id.charAt(0); // 取字串第一個字元
+		char no2 = id.charAt(1); // 取字串第二個字元
+		char no3 = id.charAt(2); // 取字串第三個字元
+		System.out.printf("出生地: %c 性別: %c 身分碼: %c%n", no1, no2, no3);
+
+		// 判斷性別 if-else
+		if(no2 == '1') {
+			System.out.println("男性");
+		} else if(no2 == '2') {
+			System.out.println("女性");
+		} else {
+			System.out.println("性別碼錯誤");
+		}
+
+		// 判斷性別 switch-case
+		switch(no2) {
+			case '1':
+				System.out.println("男性");
+				break;
+			case '2':
+				System.out.println("女性");
+				break;
+			default:
+				System.out.println("性別碼錯誤");	
+		}
+	}
+}
