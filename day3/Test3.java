3a9fc4fccb0bc2bdc0fab589d6e05fdebdb8656b
VincentTuan <vincentjava@yahoo.com.tw>
2024-07-13 10:30:49 +0800

    Add files via upload

---
+++ day3/Test3.java
@@ -0,0 +1,12 @@
+package day3;
+
+public class Test3 {
+
+	public static void main(String[] args) {
+		int age = 0;
+		age = age++;
+		System.out.printf("age = %d%n", age);
+		System.out.printf("age = %d%n", age);
+	}
+
+}
