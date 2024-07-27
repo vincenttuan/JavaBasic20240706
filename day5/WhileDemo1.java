7f44c3e0d30fa9dd8a6e4659e17f139a71f6b177
VincentTuan <vincentjava@yahoo.com.tw>
2024-07-27 10:20:22 +0800

    Update WhileDemo1.java

--- day5/WhileDemo1.java
+++ day5/WhileDemo1.java
@@ -9,9 +9,16 @@ public class WhileDemo1 {
 		Random random = new Random(); // 隨機物件
 
 		while(true) {
-
 			int n = random.nextInt(100); // 0~99 的隨機數
+
+			// 若 n 是奇數則重新執行迴圈判斷
+			// 若 n 是偶數則繼續執行程序
+			if(n % 2 == 1) {
+				continue; // 重新執行迴圈判斷
+			}
+
 			System.out.println(n); // 印出隨機數
+			
 			if(n == 0) {
 				break; // 離開迴圈
 			}
